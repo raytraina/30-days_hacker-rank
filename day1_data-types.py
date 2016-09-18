ca8ad55ff210e7bcc32ae281613a44b2f6f3aadb
Rachel Traina-Grandon <racheltrainagrandon@gmail.com>
2016-09-18 20:09:36 +0000

    Completed Day 1 Challenge on Data Types

---
+++ day1_data-types.py
@@ -0,0 +1,24 @@
+# Day 1: Data Types
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+int_2 = int()
+dub_2 = float()
+str_2 = str()
+# Read and save an integer, double, and String to your variables.
+int_2 = int(raw_input())
+dub_2 = float(raw_input())
+str_2 = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print i + int_2
+
+# Print the sum of the double variables on a new line.
+print d + dub_2
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + str_2
